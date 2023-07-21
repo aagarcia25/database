de82095391a11e48e013aaf5aff00e323b25d687
aagarcia25 <109979605+aagarcia25@users.noreply.github.com>
2023-07-21 16:21:38 -0600

    ajustes

--- PDRMYE/SCRIPT/TIcentral.sql
+++
@@ -1,2 +0,0 @@
--- relacionar usuarios a aplicaciones
-INSERT INTO TiCentral.UsuarioAplicacion('IdUsuario','IdApp') VALUES('2fad75f3-b79f-11ed-8002-d89d6776f970','7af0aa1e-70e8-11ed-a880-040300000000');

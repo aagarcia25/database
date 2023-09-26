e1a76c22c51add7c5f86f33e6a09e4eebfaa6e5d
aagarcia25 <109979605+aagarcia25@users.noreply.github.com>
2023-09-26 09:19:50 -0600

    Update cph_pef.sql

--- PDRMYE/SCRIPT/cph_pef.sql
+++ PDRMYE/SCRIPT/cph_pef.sql
@@ -1,8 +1,81 @@
-  DROP VIEW IF EXISTS `cph_pef`;
+DROP VIEW IF EXISTS `cph_pef`;
 
 CREATE VIEW
 cph_pef AS 
+SELECT * FROM (
+SELECT 
+ distinct
+        cp.id  idcalculototal,
+        ctd.id idcalculodetalle,
+        cp.Anio,
+        cp.Mes,
+        mes.Descripcion mesdescripcion,
+        mun.id idmunicipio,
+        mun.ClaveEstado,
+        mun.Nombre,
+        '02' iddtipofondo,
+        tf.Descripcion descripciontipofondo,
+        fd.id idfondo,
+        fd.Descripcion,
+        ftc.id idtipocalculo,
+         'Devoluciones' as tipocalculo,
+        TRUNCATE(AjusteEstatal,2) observadoimporte,
+        0 observadoAjusteEstatal,
+		  0 observadototalImporte,
+        0 pefanio,
+        0 peftotalImporte,
+        0 vspefs
+         
+        FROM 
+
+           PDRMYE.CalculoPrincipal cp 
+           LEFT JOIN PDRMYE.CalculoTotalDetalle ctd   ON  cp.id = ctd.idCalculoTotal AND cp.deleted=0 AND ctd.deleted=0
+           LEFT JOIN PDRMYE.Fondos fd                 ON fd.id = cp.ClaveFondo	    
+			  LEFT JOIN PDRMYE.TipoFondos tf             ON tf.id = fd.Tipo					
+			  LEFT JOIN PDRMYE.TipoFondosCalculo ftc     ON cp.idtipo = ftc.id
+           LEFT JOIN PDRMYE.Municipios mun            ON ctd.IdMun = mun.id
+           LEFT JOIN PDRMYE.Meses mes                 ON mes.mes = cp.Mes
+          WHERE AjusteEstatal <> 0 
+          AND  fd.clave='ISR SALARIOS'
+          UNION all
  SELECT 
+ distinct
+        cp.id  idcalculototal,
+        ctd.id idcalculodetalle,
+        cp.Anio,
+        cp.Mes,
+        mes.Descripcion mesdescripcion,
+        mun.id idmunicipio,
+        mun.ClaveEstado,
+        mun.Nombre,
+        '01' iddtipofondo,
+        tf.Descripcion descripciontipofondo,
+        fd.id idfondo,
+        fd.Descripcion,
+        ftc.id idtipocalculo,
+        'Ajuste Estatal' as tipocalculo,
+        TRUNCATE(AjusteEstatal,2) observadoimporte,
+        0 observadoAjusteEstatal,
+		  0 observadototalImporte,
+        0 pefanio,
+        0 peftotalImporte,
+        0 vspefs
+         
+        FROM 
+
+           PDRMYE.CalculoPrincipal cp 
+           LEFT JOIN PDRMYE.CalculoTotalDetalle ctd   ON  cp.id = ctd.idCalculoTotal AND cp.deleted=0 AND ctd.deleted=0
+           LEFT JOIN PDRMYE.Fondos fd                 ON fd.id = cp.ClaveFondo	    
+			  LEFT JOIN PDRMYE.TipoFondos tf             ON tf.id = fd.Tipo					
+			  LEFT JOIN PDRMYE.TipoFondosCalculo ftc     ON cp.idtipo = ftc.id
+           LEFT JOIN PDRMYE.Municipios mun            ON ctd.IdMun = mun.id
+           LEFT JOIN PDRMYE.Meses mes                 ON mes.mes = cp.Mes
+          WHERE AjusteEstatal <> 0 
+          AND  fd.clave<>'ISR SALARIOS'
+                                                     
+     UNION all
+     
+     SELECT 
  distinct
         cp.id  idcalculototal,
         ctd.id idcalculodetalle,
@@ -19,11 +92,11 @@ cph_pef AS
         ftc.id idtipocalculo,
         ftc.Alias as tipocalculo,
         TRUNCATE(Mensual,2) observadoimporte,
-        TRUNCATE(AjusteEstatal,2) observadoAjusteEstatal,
-		  TRUNCATE((ctd.Mensual     + ctd.AjusteEstatal ),2) observadototalImporte,
+        0 observadoAjusteEstatal,
+		  TRUNCATE(ctd.Mensual  ,2) observadototalImporte,
         pefe.Anio pefanio,
         TRUNCATE(pefe.importe,2) peftotalImporte,
-        TRUNCATE(pefe.importe,2) - TRUNCATE((ctd.Mensual     + ctd.AjusteEstatal ),2) vspefs
+        TRUNCATE(pefe.importe,2) - TRUNCATE(ctd.Mensual  ,2) vspefs
          
         FROM 
 
@@ -37,8 +110,11 @@ cph_pef AS
            LEFT JOIN PDRMYE.PEF pefe                  ON pefe.mes = cp.Mes
                                                       AND pefe.Anio = cp.Anio
                                                       AND pefe.ClaveFondo = cp.ClaveFondo 
-                                                      AND pefe.idMunicipio = mun.id
-     
-	 ORDER BY cp.Anio,
-           cp.Mes asc;
+                                                      AND pefe.idMunicipio = ctd.IdMun AND  ftc.Alias ='Mensual'
+                                                     
+                                                     
+  
+    )tbl
+    -- WHERE  tbl.Anio=2023 AND tbl.Mes=2 AND tbl.ClaveEstado=70 
+	 ORDER BY tbl.Anio,           tbl.Mes asc;
            
